8792b4883071584d5969fc6ad7e32227a6d4d85d
Will Wedmedyk <willwedmedyk@gmail.com>
2024-03-21 20:53:51 -0400

    ww/practicing memory of bst

--- js/bst_memory_test.js
+++ js/bst_memory_test.js
@@ -1,5 +1,4 @@
 class Node {
-
 	constructor(data) {
 		this.data = data;
 		this.right = null;
@@ -18,23 +17,75 @@ class BST {
 			this.root = node;
 			return node;
  		} else {
-			return insertNewNode(this.root, node);
+			return this.insertNewNode(this.root, node);
 		}
 	}
 
 	insertNewNode(node, newNode){	
-		if(node.data < newNode.data) {
+		if(newNode.data < node.data) {
 			if(node.left === null) {
 				node.left = newNode;
 			} else {
-				insertNewNode(node.left, newNode);
+				this.insertNewNode(node.left, newNode);
 			}
-		} else if (node.data > this.root) {
-				if(node.right === null) {
-					node.right === newNode;
-				} else {
-					insertNewNode(node.right, newNode);
-				}
+		} else if (newNode.data > node.data) {
+			if(node.right === null) {
+				node.right = newNode;
+			} else {
+				this.insertNewNode(node.right, newNode);
+			}
+		}
+	}
+	delete(data) {
+		this.root = this.removeNode(this.root, data);
+	}
+
+	removeNode(node, key) {
+		if(node === null) return node;
+
+		if (key < node.val) {
+			node.left = this.removeNode(node.left, key)
+			return node;
+		} else if(key > node.val) {
+			node.right = this.removeNode(node.right, key)
+			return node;
+		} else {
+			if(node.right === null && node.left === null) {
+				node = null;
+				return node;
+			}
+
+			if(node.left === null) {
+				node.right = null;
+				return node;
+			} else if(node.right === null) {
+				node.left = null;
+				return node
+			}
+
+			const aux = this.findMinNode(node.right)
+			node.data = aux.data;
+
+			node.right = this.removeNode(node.right, aux.data);
+			return node;
+		}
+	}
+
+	findMinNode(node) {
+		if(node.left === null) {
+		return node;
+		} else {
+			return this.findMinNode(node.left);
 		}
 	}
 }
+
+
+const tree = new BST;
+tree.insert(8);
+tree.insert(3);
+tree.insert(11);
+tree.insert(1);
+tree.insert(5);
+tree.insert(9)
+console.log("tree:", tree);
